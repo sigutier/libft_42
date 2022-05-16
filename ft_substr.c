c0dfe3d0f3bb8a99515bdef51381bc67018cd944
Silvia Gutierrez Rodriguez <98970694+sigutier@users.noreply.github.com>
2022-05-16 21:32:51 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -12,62 +12,28 @@
 
 #include "libft.h"
 
-/*char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	 char	*sub;
-	 size_t	size;
-	 size_t	i;
-	 size_t	str_len;
-
-	 str_len = ft_strlen(s);
-	 if (s == NULL)
-		 return (NULL);
-	 if (str_len < start)
-	 {
-		 sub = (char *)malloc(1 * sizeof(char));
-		 if (!sub)
-			 return (NULL);
-		 *sub = '\0';
-		 return (sub);
-	 }
-	 if (str_len > len)
-		 size = len;
-	 else
-		 size = str_len;
-	 sub = (char *)malloc((size + 1) * sizeof(char));
-	 if (sub == NULL)
-		 return (NULL);
-	 i = 0;
-	 while (i < size)
-	{
-		sub[i] = s[start];
-		i++;
-		start++;
-	}
-	sub[i] = '\0';
-	return (sub);
-}*/
-
 char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
 	char	*sub;
-
-	sub = (char *)malloc((len + 1) * sizeof(*s));
+	
+	if (!s)
+		return (NULL);
+	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (s[i])
 	{
-		if (i >= start && j < len)
+		if (i >= start && i < (start + len))
 		{
 			sub[j] = s[i];
 			j++;
 		}
 		i++;
 	}
-	sub[j] = 0;
+	sub[j] = '\0';
 	return (sub);
 }
